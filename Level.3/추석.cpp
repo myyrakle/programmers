b22bc27df71f4951da08adafb7ecad9d5327f070
myyrakle <sssang97@naver.com>
2020-07-19 17:50:34 +0900

    Update 추석.cpp

--- Level.3/추석.cpp
+++ Level.3/추석.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-pair<long long, long long> to_converted_pair(const std::string &text)
+auto to_converted_pair(const std::string &text) -> pair<long long, long long>
 {
     int year;
     int month;
@@ -18,13 +18,14 @@ pair<long long, long long> to_converted_pair(const std::string &text)
     int minute;
     double second;
 
-    double processing_time;
+    double processing_time; //처리시간
 
     sscanf(text.data(), "%d-%d-%d %d:%d:%lf %lfs", &year, &month, &day, &hour, &minute, &second, &processing_time);
 
-    int millisecond = fmod(second, 1) * 1000;
+    int millisecond = int(second * 1000) % 1000;
 
-    long long end_time = millisecond + (int(second / 1) * 1000) + (minute * 1000 * 60) + (hour * 1000 * 60 * 60) + (day * 1000 * 60 * 60 * 24); // + (year * 1000 * 60 * 60 * 24 * 365);
+    long long end_time = millisecond + (int(second / 1) * 1000) + (minute * 1000 * 60) + (hour * 1000 * 60 * 60) + (day * 1000 * 60 * 60 * 24) + (month * 1000 * 60 * 60 * 24 * 31) + (year * 1000 * 60 * 60 * 24 * 365);
+
     long long start_time = end_time - int(processing_time * 1000) + 1;
 
     return make_pair(start_time, end_time);
@@ -41,68 +42,51 @@ int solution(vector<string> lines)
     }
 
     int max = 0;
-    for (const auto &e : converted)
+    for (int i = 0; i < converted.size(); i++)
     {
-        const auto start = e.first;
-        const auto end = e.second;
+        {
+            const auto start = converted[i].second;
+            const auto end = start + 999;
 
-        // const auto start_count = count_if(
-        //     converted.begin(), converted.end(),
-        //     [&start](pair<long long, long long> &e) {
-        //         return (start <= e.first && e.first <= start + 999) || (start <= e.second && e.second <= start + 999);
-        //     });
+            int count = 0;
+            for (int j = 0; j < converted.size(); j++)
+            {
+                const auto each_start = converted[j].first;
+                const auto each_end = converted[j].second;
 
-        // max = start_count > max ? start_count : max;
+                if (
+                    (start <= each_start && each_start <= end) ||
+                    (start <= each_end && each_end <= end))
+                {
+                    count++;
+                }
+            }
 
-        const auto end_count = count_if(
-            converted.begin(), converted.end(),
-            [&end](pair<long long, long long> &e) {
-                return (end <= e.first && e.first <= end + 1000) || (end <= e.second && e.second <= end + 1000);
-            });
+            max = count > max ? count : max;
+        }
 
-        max = end_count > max ? end_count : max;
+        {
+            const auto end = converted[i].first;
+            const auto start = end - 999;
 
-        //break;
+            int count = 0;
+
+            for (int j = 0; j < converted.size(); j++)
+            {
+                const auto each_start = converted[j].first;
+                const auto each_end = converted[j].second;
+
+                if (
+                    (start <= each_start && each_start <= end) ||
+                    (start <= each_end && each_end <= end))
+                {
+                    count++;
+                }
+            }
+
+            max = count > max ? count : max;
+        }
     }
 
     return max;
-}
-
-int main()
-{
-    vector<string> data{"2016-09-15 20:59:57.421 0.351s"s,
-                        "2016-09-15 20:59:58.233 1.181s"s,
-                        "2016-09-15 20:59:58.299 0.8s"s,
-                        "2016-09-15 20:59:58.688 1.041s"s,
-                        "2016-09-15 20:59:59.591 1.412s"s,
-                        "2016-09-15 21:00:00.464 1.466s"s,
-                        "2016-09-15 21:00:00.741 1.581s"s,
-                        "2016-09-15 21:00:00.748 2.31s"s,
-                        "2016-09-15 21:00:00.966 0.381s"s,
-                        "2016-09-15 21:00:02.066 2.62s"s};
-
-    //"2016-09-15 01:00:04.001 2.0s", "2016-09-15 01:00:07.000 2s"
-    /* 기대값 1
-        "2016-09-15 01:00:04.001 2.0s",
-        "2016-09-15 01:00:07.000 2s"
-        */
-
-    /* 기대값 2
-     "2016-09-15 01:00:04.002 2.0s",
-     "2016-09-15 01:00:07.000 2s"
-*/
-    /*"2016-09-15 20:59:57.421 0.351s"s,
-        "2016-09-15 20:59:58.233 1.181s"s,
-        "2016-09-15 20:59:58.299 0.8s"s,
-        "2016-09-15 20:59:58.688 1.041s"s,
-        "2016-09-15 20:59:59.591 1.412s"s,
-        "2016-09-15 21:00:00.464 1.466s"s,
-        "2016-09-15 21:00:00.741 1.581s"s,
-        "2016-09-15 21:00:00.748 2.31s"s,
-        "2016-09-15 21:00:00.966 0.381s"s,
-        "2016-09-15 21:00:02.066 2.62s"s*/
-
-    // cout <<
-    cout
-        << solution(data);
 }
