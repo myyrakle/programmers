398b55e46989e21033d034028b34ccb5e837083a
myyrakle <sssang97@naver.com>
2020-08-04 08:28:34 +0900

    Create 전화번호 목록.cpp
    failed

---
+++ Level.2/전화번호 목록.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <regex>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) 
+{
+    bool answer = true;
+    
+    for(int i = 0; i<phone_book.size(); i++) 
+    {
+        regex search_text{"^"+phone_book[i]+"*"};
+        
+        for(int j = 0; j<phone_book.size(); j++)
+        {
+            if(i==j)
+            {
+                continue;
+            }
+            
+            if(regex_search(phone_book[j], search_text))
+            {
+                answer = false;
+                goto FOR_OUT;
+            }
+        }
+    }
+    FOR_OUT:
+    
+    return answer;
+}
