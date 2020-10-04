67114542508e37da8e9a3c5986d7f043c9c00611
myyrakle <sssang97@naver.com>
2020-10-05 08:12:43 +0900

    failed

--- Level.2/가장 큰 수.js
+++ Level.2/가장 큰 수.js
@@ -1,3 +1,19 @@
 function solution(numbers) {
-    return numbers.map(e=>String(e)).sort().reverse().reduce((acc, e)=>acc+e, '');
+    return numbers.map(e=>String(e)).sort(
+        (l, r)=>{
+            let left = l;
+            while(left.length<4){
+                left+=l;
+            }
+            left = left.substring(0, 4);
+            
+            let right = r;
+            while(right.length<4){
+                right+=r;
+            }
+            right = right.substring(0, 4);
+            
+            return left.localeCompare(right);
+        }
+    ).reverse().reduce((acc, e)=>acc+e, '');
 }
