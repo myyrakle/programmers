890e41ec7200f12d63f3821f8ef68fa041d1a91a
myyrakle <sssang97@naver.com>
2020-10-06 08:06:15 +0900

    성공

--- Level.2/파일명 정렬.js
+++ Level.2/파일명 정렬.js
@@ -1,7 +1,7 @@
 function solution(files) {
     return files.sort((l, r)=>{
-        const lHead = /[a-zA-Z]*/.exec(l.toUpperCase());
-        const rHead = /[a-zA-Z]*/.exec(r.toUpperCase());
+        const lHead = /[a-zA-Z, -]*/.exec(l.toUpperCase());
+        const rHead = /[a-zA-Z, -]*/.exec(r.toUpperCase());
         
         if(lHead < rHead){
             return -1;
@@ -10,8 +10,8 @@ function solution(files) {
             return 1;
         }
         
-        const lNumber = l.match(/[0-9]+/);
-        const rNumber = r.match(/[0-9]+/);
+        const lNumber = parseInt(l.match(/[0-9]+/));
+        const rNumber = parseInt(r.match(/[0-9]+/));
         return lNumber - rNumber;
     });
 }
